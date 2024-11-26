ccf1ac86dd3d35c04592e37750404d993e3f1e73
ahmed <ao1922302@gmail.com>
2024-11-26 15:04:36 +0200

    add env file to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:store_app/core/app/env_variables.dart';
+import 'package:store_app/firebase_options.dart';
 import 'package:store_app/store_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const StoreApp());
+
+  await EnvVariables.instance.init(envTypeEnum: EnvTypeEnum.dev);
 }
